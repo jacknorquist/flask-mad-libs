daeacc868213e9d84df2471c54c407feba9eae69
Jack Norquist <jack.norquist@outlook.com>
2024-02-21 17:42:31 -0600

    Story Works

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def set_story():
+    prompts = silly_story.prompts
+    return render_template("questions.html", prompts=prompts)
+
+@app.get('/results')
+def show_results():
+   story = silly_story.get_result_text(request.args)
+   return render_template("results.html", story=story)
+
