7c8f4b7d592136a9f04ac64b9d289b737fd04e63
Thomas Kim <tkimm156@gmail.com>
2024-02-21 19:07:59 -0600

    get adlibs working

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
+
+STORIES = {"Silly Story" : silly_story, "Excited Story" : excited_story}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+@app.get("/")
+def choose_story():
+    return render_template("stories.html", stories = STORIES)
 
-@app.get('/')
-def set_story():
-    prompts = silly_story.prompts
-    return render_template("questions.html", prompts=prompts)
+@app.get('/questions')
+def show_story_form():
+    """Show madlibs form based on Story instance"""
 
-@app.get('/results')
+    prompts = request.args
+    return render_template("questions.html", prompts = prompts)
+
+
+@app.get('/<story>/results')
 def show_results():
-   story = silly_story.get_result_text(request.args)
-   return render_template("results.html", story=story)
+    """Create a story based on form query parameters"""
 
+    story = silly_story.get_result_text(request.args)
+    return render_template("results.html", story = story)
+
+#a=request.args.get("a")
